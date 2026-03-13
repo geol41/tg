2be04aeeb6c943b4b255c1b6d188009a1079f261
geol41 <6a6ka42@gmail.com>
2026-03-13 13:03:20 +0300

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,13 +2,6 @@ export default {
   async fetch(request) {
     const url = new URL(request.url);
     const actualUrl = 'https://api.telegram.org' + url.pathname + url.search;
-    
-    const modifiedRequest = new Request(actualUrl, {
-      method: request.method,
-      headers: request.headers,
-      body: request.body
-    });
-    
-    return fetch(modifiedRequest);
+    return fetch(actualUrl, request);
   }
 }
